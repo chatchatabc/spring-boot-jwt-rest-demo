4de3bab4e18606b595a4944d1f0fb90185b629ce
Libing Chen <libing.chen@gmail.com>
2023-05-21 15:29:50 +0800

    chore: extends TestcontainersBaseTest

--- src/test/java/com/chatchatabc/store/book/BookSupportBaseTest.java
+++ src/test/java/com/chatchatabc/store/book/BookSupportBaseTest.java
@@ -1,13 +1,13 @@
 package com.chatchatabc.store.book;
 
-import com.chatchatabc.store.BookStoreBaseTest;
+import com.chatchatabc.store.TestcontainersBaseTest;
 import com.chatchatabc.store.book.domain.model.Book;
 import com.chatchatabc.store.book.domain.repository.BookRepository;
 import com.github.database.rider.core.api.dataset.DataSet;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @DataSet("db/datasets/book.xml")
-public abstract class BookSupportBaseTest extends BookStoreBaseTest {
+public abstract class BookSupportBaseTest extends TestcontainersBaseTest {
     @Autowired
     protected BookRepository bookRepository;
 
