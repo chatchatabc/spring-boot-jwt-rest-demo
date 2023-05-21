b9aab7ed46fc8d4ec1711e88a88a7a532e60816d
Libing Chen <libing.chen@gmail.com>
2023-05-21 15:29:31 +0800

    chore: polishing

--- src/test/java/com/chatchatabc/store/book/domain/repository/BookRepositoryTest.java
+++ src/test/java/com/chatchatabc/store/book/domain/repository/BookRepositoryTest.java
@@ -1,13 +1,17 @@
 package com.chatchatabc.store.book.domain.repository;
 
 import com.chatchatabc.store.book.BookSupportBaseTest;
+import com.chatchatabc.store.book.domain.model.Book;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class BookRepositoryTest extends BookSupportBaseTest {
 
     @Test
     public void testFindById() throws Exception {
-
+        final Book book = getBook(1L);
+        assertThat(book).isNotNull();
     }
 
 }
