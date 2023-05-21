11d0daa5e019f5c855c83fe1ac76a8400012db2d
Libing Chen <libing.chen@gmail.com>
2023-05-21 13:39:45 +0800

    chore: add show()

--- src/main/java/com/chatchatabc/store/book/application/rest/BookController.java
+++ src/main/java/com/chatchatabc/store/book/application/rest/BookController.java
@@ -1,14 +1,24 @@
 package com.chatchatabc.store.book.application.rest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.chatchatabc.store.book.domain.model.Book;
+import com.chatchatabc.store.book.domain.repository.BookRepository;
+import com.chatchatabc.store.operator.application.common.OperatorPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/book")
 public class BookController {
-    private static final Logger log = LoggerFactory.getLogger(BookController.class);
+    @Autowired
+    private BookRepository bookRepository;
 
-
+    @GetMapping("/{id}")
+    public Optional<Book> show(OperatorPrincipal principal, @PathVariable("id") Long id) {
+        return bookRepository.findById(id);
+    }
 }
